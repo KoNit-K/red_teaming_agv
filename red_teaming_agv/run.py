0ba9c1ee3526aae74ad2385f9026ceea82bfea7c
Konit-K <konit.block@protonmail.com>
2025-03-15 17:01:57 +0800

    style: merge target agent file

--- red_teaming_agv/run.py
+++ red_teaming_agv/run.py
@@ -10,7 +10,6 @@ from naptha_sdk.modules.agent import Agent
 from naptha_sdk.schemas import AgentRunInput, AgentDeployment
 from naptha_sdk.user import sign_consumer_id
 
-from red_teaming_agv.chat_agent import ChatAgent
 from red_teaming_agv.common.Target import Target
 from red_teaming_agv.prompt import get_attacker_system_prompt
 from red_teaming_agv.schemas import InputSchema, SystemPromptSchema, ChatInputSchema
@@ -21,6 +20,42 @@ from red_teaming_agv.adv_eva import EvaluatorAgent
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
+
+class ChatAgent:
+
+    def __init__(self, deployment: AgentDeployment):
+        self.deployment = deployment
+        if deployment.config.persona_module and deployment.config.system_prompt["persona"]:
+            self.system_prompt = SystemPromptSchema(role=deployment.config.system_prompt["role"], persona=deployment.config.system_prompt["persona"])
+        else:
+            self.system_prompt = SystemPromptSchema(role=deployment.config.system_prompt["role"])
+        self.node = InferenceClient(self.deployment.node)
+
+    async def chat(self, inputs: InputSchema):
+        if isinstance(inputs.tool_input_data, list):
+            messages = [msg for msg in inputs.tool_input_data if msg["role"] != "system"]
+        elif isinstance(inputs.tool_input_data, str):
+            messages = [{"role": "user", "content": inputs.tool_input_data}]
+        messages.insert(0, {"role": "system", "content": json.dumps(self.deployment.config.system_prompt)})
+
+        response = await self.node.run_inference({"model": self.deployment.config.llm_config.model,
+                                                    "messages": messages,
+                                                    "temperature": self.deployment.config.llm_config.temperature,
+                                                    "max_tokens": self.deployment.config.llm_config.max_tokens})
+
+        if isinstance(response, dict):
+            response = response['choices'][0]['message']['content']
+        else:
+            response = response.choices[0].message.content
+        logger.info(f"Response: {response}")
+
+        messages.append({"role": "assistant", "content": response})
+
+        messages = [msg for msg in messages if msg["role"] != "system"]
+
+        return messages
+
+
 
 class RedTeamingAgent(Agent):
     def __init__(self, deployment: AgentDeployment):
